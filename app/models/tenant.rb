f91c4747891bcd61d80e4f00c675fb831f1b94e5
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-03-02 03:41:49 +0000

    add projects

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -2,6 +2,7 @@ class Tenant < ActiveRecord::Base
 
    acts_as_universal_and_determines_tenant
   has_many :members, dependent: :destroy
+  has_many :projects, dependent: :destroy
   validates_uniqueness_of :name
   validates_presence_of :name
 
