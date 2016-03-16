eb7e392d4b4c2922b81f36a8e659f27023aab370
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-03-16 14:20:59 +0000

    fix aws issue

--- app/controllers/artifacts_controller.rb
+++ app/controllers/artifacts_controller.rb
@@ -29,9 +29,8 @@ class ArtifactsController < ApplicationController
 
     respond_to do |format|
       if @artifact.save
-        format.html { 
-          redirect_to tenant_project_url(tenant_id: Tenant.current_tenant_id, id: @artifact.project_id), 
-          notice: 'Artifact was successfully created.' }
+        format.html { redirect_to tenant_project_url(tenant_id: Tenant.current_tenant_id, 
+                id: @artifact.project_id), notice: 'Artifact was successfully created.' }
         format.json { render :show, status: :created, location: @artifact }
       else
         format.html { render :new }
@@ -59,9 +58,7 @@ class ArtifactsController < ApplicationController
   def destroy
     @artifact.destroy
     respond_to do |format|
-      format.html { redirect_to tenant_project_url(tenant_id: Tenant.current_tenant_id, 
-      id: @artifact.project_id),
-      notice: 'Artifact was successfully destroyed.' }
+      format.html { redirect_to tenant_project_url(tenant_id: Tenant.current_tenant_id, id: @artifact.project_id), notice: 'Artifact was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -1,4 +1,5 @@
 class Artifact < ActiveRecord::Base
+  
   before_save :upload_to_s3
   attr_accessor :upload
   belongs_to :project
@@ -6,8 +7,9 @@ class Artifact < ActiveRecord::Base
   MAX_FILESIZE = 10.megabytes
   validates_presence_of :name, :upload
   validates_uniqueness_of :name
-  validate :uploaded_file_size
-
+  
+  validate :uploaded_fize_size
+  
   private
   
   def upload_to_s3
@@ -15,12 +17,12 @@ class Artifact < ActiveRecord::Base
     tenant_name = Tenant.find(Thread.current[:tenant_id]).name
     obj = s3.bucket(ENV['AWS_S3_BUCKET']).object("#{tenant_name}/#{upload.original_filename}")
     obj.upload_file(upload.path, acl:'public-read')
-    self.key = obj.public_url 
+    self.key = obj.public_url
   end
   
-  def uploaded_file_size
-    if upload
-    errors.add(:upload, "File size must be less than #{self.class::MAX_FILESIZE}") unless upload.size <= self.class::MAX_FILESIZE
+  def uploaded_fize_size
+    if upload 
+      errors.add(:upload, "File size must be less than #{self.class::MAX_FILESIZE}") unless upload.size <= self.class::MAX_FILESIZE
     end
   end
 end
